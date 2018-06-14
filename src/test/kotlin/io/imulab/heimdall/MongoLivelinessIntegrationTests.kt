9b59f90fb3f76e09a088242c3bcd80a5ad404399
Weinan Qiu <davidiamyou@gmail.com>
2018-06-14 13:38:30 -0400

    circleci: add mongodb

--- src/test/kotlin/io/imulab/heimdall/MongoLivelinessIntegrationTests.kt
+++ src/test/kotlin/io/imulab/heimdall/MongoLivelinessIntegrationTests.kt
@@ -19,7 +19,8 @@ class MongoLivelinessIntegrationTests {
 
     @BeforeEach
     fun setup(vtx: Vertx, tc: VertxTestContext) {
-        mongo = MongoClient.createShared(vtx, JsonObject().put("connection_string", "mongodb://localhost:32770"))
+        mongo = MongoClient.createShared(vtx, JsonObject().put("connection_string",
+                System.getProperty("TEST_MONGO_URI", "mongodb://localhost:32770")))
         tc.completeNow()
     }
 
