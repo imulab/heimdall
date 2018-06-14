ec7c340ddbbee32a39d2d13a1d6be42e0b9a3a31
Weinan Qiu <davidiamyou@gmail.com>
2018-06-14 14:02:21 -0400

    circleci: add mongodb #5

--- src/test/kotlin/io/imulab/heimdall/MongoLivelinessIntegrationTests.kt
+++ src/test/kotlin/io/imulab/heimdall/MongoLivelinessIntegrationTests.kt
@@ -3,6 +3,7 @@ package io.imulab.heimdall
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.mongo.MongoClient
+import io.vertx.junit5.Timeout
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import org.assertj.core.api.Assertions.*
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.concurrent.TimeUnit
 
 @Tag("integration")
 @ExtendWith(VertxExtension::class)
@@ -19,13 +21,14 @@ class MongoLivelinessIntegrationTests {
 
     @BeforeEach
     fun setup(vtx: Vertx, tc: VertxTestContext) {
-        println(System.getenv("TEST_MONGO_URI") ?: "mongodb://localhost:32770")
+        println(System.getenv("TEST_MONGO_URI") ?: "mongodb://localhost:27017")
         mongo = MongoClient.createShared(vtx, JsonObject().put("connection_string",
-                System.getenv("TEST_MONGO_URI") ?: "mongodb://localhost:32770"))
+                System.getenv("TEST_MONGO_URI") ?: "mongodb://localhost:27017"))
         tc.completeNow()
     }
 
     @Test
+    @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
     fun mongoDBIsAlive(vtx: Vertx, tc: VertxTestContext) {
         mongo.runCommand("ping", JsonObject().put("ping", 1), tc.succeeding {
             assertThat(it.getDouble("ok")).isEqualTo(1.0)
