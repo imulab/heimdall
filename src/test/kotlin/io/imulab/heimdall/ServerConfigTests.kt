a102bcd14deac7839756c9d55ffb9c8d52a24a9f
Weinan Qiu <davidiamyou@gmail.com>
2018-06-20 16:08:10 -0400

    fix concurrency issue: update ServerConfigTests to sequential execution using locks

--- src/test/kotlin/io/imulab/heimdall/ServerConfigTests.kt
+++ src/test/kotlin/io/imulab/heimdall/ServerConfigTests.kt
@@ -1,113 +1,123 @@
 package io.imulab.heimdall
 
 import io.vertx.core.Vertx
-import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import io.vertx.junit5.Timeout
 import io.vertx.junit5.VertxExtension
-import io.vertx.junit5.VertxTestContext
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
 
-@ExtendWith(VertxExtension::class)
+/**
+ * Tests for ServerConfig.
+ *
+ * ServerConfig is not designed for concurrent write scenario, hence we need to control the execution order
+ * of the tests. In particular, any concurrent execution of tests should not be allowed. In order to achieve
+ * sequential execution, we use an ReentrantLock to limit the number of parties that can modify the state of
+ * ServerConfig to only one.
+ *
+ * @see ServerConfigTests#withLock()
+ */
 class ServerConfigTests {
+
+    private val lock = ReentrantLock()
+
+    private fun withLock(test: () -> Unit) {
+        lock.lock()
+        try {
+            test()
+        } finally {
+            ServerConfig.reset()
+            lock.unlock()
+        }
+    }
 
     @Test
     @Timeout(value = 30, timeUnit = TimeUnit.SECONDS)
     @DisplayName("Test loading configuration from a YAML file")
-    fun testLoadYaml(vtx: Vertx, tc: VertxTestContext) {
-        ServerConfig.load(vertx = vtx, yamlPaths = setOf("server-config-test.yaml"))
-                .subscribe({
-                    assertThat(ServerConfig.config()
-                            .getJsonObject("foo")
-                            .getString("bar"))
-                            .isEqualTo("hello")
-                    assertThat(ServerConfig.config()
-                            .getJsonObject("foo")
-                            .getJsonArray("baz"))
-                            .contains("x", "y", "z")
-                    tc.completeNow()
-                }, tc::failNow)
+    fun testLoadYaml() {
+        withLock {
+            ServerConfig.load(vertx = Vertx.vertx(), yamlPaths = setOf("server-config-test.yaml")).blockingGet()
+            assertThat(ServerConfig.config()
+                    .getJsonObject("foo")
+                    .getString("bar"))
+                    .isEqualTo("hello")
+            assertThat(ServerConfig.config()
+                    .getJsonObject("foo")
+                    .getJsonArray("baz"))
+                    .contains("x", "y", "z")
+        }
     }
 
     @Test
     @Timeout(value = 30, timeUnit = TimeUnit.SECONDS)
     @DisplayName("Test loading configuration from the default source")
-    fun testLoadDefault(vtx: Vertx, tc: VertxTestContext) {
-        ServerConfig.load(vertx = vtx, defaults = JsonObject().put("foo", "bar"))
-                .subscribe({ _ ->
-                    assertThat(ServerConfig.config().getString("foo"))
-                            .isEqualTo("bar")
-                    tc.completeNow()
-                }, tc::failNow)
+    fun testLoadDefault() {
+        withLock {
+            ServerConfig.load(vertx = Vertx.vertx(), defaults = JsonObject().put("foo", "bar")).blockingGet()
+            assertThat(ServerConfig.config().getString("foo"))
+                    .isEqualTo("bar")
+        }
     }
 
     @Test
     @Timeout(value = 30, timeUnit = TimeUnit.SECONDS)
     @DisplayName("Test loading configuration from environment variable. (Assuming \$HOME is set)")
-    fun testLoadEnv(vtx: Vertx, tc: VertxTestContext) {
-        ServerConfig.load(vertx = vtx, envKeys = setOf("HOME"))
-                .subscribe({ _ ->
-                    assertThat(ServerConfig.config()
-                            .getString("HOME"))
-                            .isNotBlank()
-                    tc.completeNow()
-                }, tc::failNow)
+    fun testLoadEnv() {
+        withLock {
+            ServerConfig.load(vertx = Vertx.vertx(), envKeys = setOf("HOME")).blockingGet()
+            assertThat(ServerConfig.config()
+                    .getString("HOME"))
+                    .isNotBlank()
+        }
     }
 
     @Test
     @Timeout(value = 30, timeUnit = TimeUnit.SECONDS)
     @DisplayName("Test loading configuration twice without reloading, second time should pass.")
-    fun testLoadTwice(vtx: Vertx, tc: VertxTestContext) {
-        ServerConfig.load(vtx)
-                .flatMap{ ServerConfig.load(vtx, yamlPaths = setOf("server-config-test.yaml")) }
-                .subscribe({
-                    assertThat(ServerConfig.config()).isEmpty()
-                    tc.completeNow()
-                }, tc::failNow)
+    fun testLoadTwice() {
+        withLock {
+            val vtx = Vertx.vertx()
+            ServerConfig.load(vtx)
+                    .flatMap{ ServerConfig.load(vtx, yamlPaths = setOf("server-config-test.yaml")) }
+                    .blockingGet()
+            assertThat(ServerConfig.config()).isEmpty()
+        }
     }
 
     @Test
     @Timeout(value = 30, timeUnit = TimeUnit.SECONDS)
     @DisplayName("Test overriding configuration entries")
-    fun testLoadOverride(vtx: Vertx, tc: VertxTestContext) {
-        ServerConfig.load(vertx = vtx,
-                defaults = JsonObject().put("foo", "bar"),
-                yamlPaths = setOf("server-config-test.yaml"))
-                .subscribe({
-                    assertThat(ServerConfig.config()
-                            .getJsonObject("foo")
-                            .getString("bar"))
-                            .isEqualTo("hello")
-                    assertThat(ServerConfig.config()
-                            .getJsonObject("foo")
-                            .getJsonArray("baz"))
-                            .contains("x", "y", "z")
-                    tc.completeNow()
-                }, tc::failNow)
+    fun testLoadOverride() {
+        withLock {
+            ServerConfig.load(vertx = Vertx.vertx(),
+                    defaults = JsonObject().put("foo", "bar"),
+                    yamlPaths = setOf("server-config-test.yaml")).blockingGet()
+            assertThat(ServerConfig.config()
+                    .getJsonObject("foo")
+                    .getString("bar"))
+                    .isEqualTo("hello")
+            assertThat(ServerConfig.config()
+                    .getJsonObject("foo")
+                    .getJsonArray("baz"))
+                    .contains("x", "y", "z")
+        }
     }
 
     @Test
     @Timeout(value = 30, timeUnit = TimeUnit.SECONDS)
     @DisplayName("Test overriding configuration entries")
-    fun testGetProperty(vtx: Vertx, tc: VertxTestContext) {
-        ServerConfig.load(vertx = vtx,
-                yamlPaths = setOf("server-config-test.yaml"))
-                .subscribe({
-                    assertThat(ServerConfig.property("foo.bar")).isEqualTo("hello")
-                    assertThat(ServerConfig.propertyAsString("foo.bar")).isEqualTo("hello")
-                    assertThat(ServerConfig.property("foo.baz") as Iterable<*>)
-                            .contains("x", "y", "z")
-                    tc.completeNow()
-                }, tc::failNow)
-    }
-
-    @AfterEach
-    fun cleanUp() {
-        ServerConfig.reset()
+    fun testGetProperty() {
+        withLock {
+            ServerConfig.load(vertx = Vertx.vertx(),
+                    yamlPaths = setOf("server-config-test.yaml")).blockingGet()
+            assertThat(ServerConfig.property("foo.bar")).isEqualTo("hello")
+            assertThat(ServerConfig.propertyAsString("foo.bar")).isEqualTo("hello")
+            assertThat(ServerConfig.property("foo.baz") as Iterable<*>)
+                    .contains("x", "y", "z")
+        }
     }
 }
